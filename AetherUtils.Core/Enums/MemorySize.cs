8bd6e632f1f6d9389eae0193b3a5f61e15a3894a
Ethan Hann <36464732+ethan-hann@users.noreply.github.com>
2024-02-12 18:38:50 -0600

    Update MemorySize.cs

--- AetherUtils.Core/Enums/MemorySize.cs
+++ AetherUtils.Core/Enums/MemorySize.cs
@@ -7,24 +7,45 @@ namespace AetherUtils.Core.Enums;
 /// </summary>
 public enum MemorySize : ulong
 {
+    /// <summary>
+    /// Represents 1 Byte.
+    /// </summary>
     [Description("Byte")]
     Byte = 1,
     
+    /// <summary>
+    /// Represents 1 Kilobyte.
+    /// </summary>
     [Description("KB")]
     KiloByte = Byte * 1024,
     
+    /// <summary>
+    /// Represents 1 MegaByte.
+    /// </summary>
     [Description("MB")]
     MegaByte = KiloByte * 1024,
     
+    /// <summary>
+    /// Represents 1 GigaByte.
+    /// </summary>
     [Description("GB")]
     GigaByte = MegaByte * 1024,
     
+    /// <summary>
+    /// Represents 1 TerraByte.
+    /// </summary>
     [Description("TB")]
     TerraByte = GigaByte * 1024,
     
+    /// <summary>
+    /// Represents 1 PetaByte.
+    /// </summary>
     [Description("PB")]
     PetaByte = TerraByte * 1024,
     
+    /// <summary>
+    /// Represents 1 ExaByte.
+    /// </summary>
     [Description("EB")]
     ExaByte = PetaByte * 1024
 }
