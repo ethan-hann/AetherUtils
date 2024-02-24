eae8c57c49e1851f291db0b8ef85b08096fe0fa2
Ethan Hann <36464732+ethan-hann@users.noreply.github.com>
2024-02-24 14:34:48 -0600

    Update KeyPair to use a class instead of a struct

--- AetherUtils.Core/Licensing/KeyPair.cs
+++ AetherUtils.Core/Licensing/KeyPair.cs
@@ -1,20 +1,27 @@
-﻿namespace AetherUtils.Core.Licensing
+﻿using AetherUtils.Core.Structs;
+
+namespace AetherUtils.Core.Licensing
 {
     /// <summary>
     /// Stores a read-only pair of keys.
     /// </summary>
     /// <param name="privateKey">The private key.</param>
     /// <param name="publicKey">The public key.</param>
-    public readonly struct KeyPair(string privateKey, string publicKey)
+    public sealed class KeyPair(string publicKey, string privateKey)
     {
-        /// <summary>
-        /// The private key component of the key pair.
-        /// </summary>
-        public string PrivateKey { get; } = privateKey;
-        
         /// <summary>
         /// The public key component of the key pair.
         /// </summary>
         public string PublicKey { get; } = publicKey;
+        
+        /// <summary>
+        /// The private key component of the key pair.
+        /// </summary>
+        public string PrivateKey { get; } = privateKey;
+
+        /// <summary>
+        /// Get a read-only pair containing the public and private key.
+        /// </summary>
+        public ReadOnlyPair<string, string> AsReadOnlyPair => new(PublicKey, PrivateKey);
     }
 }
--- AetherUtils.Core/Validation/IValidator.cs
+++ AetherUtils.Core/Validation/IValidator.cs
@@ -9,7 +9,6 @@ public interface IValidator<in T> where T : notnull
     /// Validate the objects against a set of conditions.
     /// </summary>
     /// <param name="objects">The objects to validate.</param>
-    /// <typeparam name="T">The type of the object.</typeparam>
     /// <returns>A list of <see cref="IValidationFailure"/> indicating the result of validation.
     /// If no validation errors, this list should be empty.</returns>
     public static abstract List<IValidationFailure> Validate(params T[] objects);
